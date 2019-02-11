ce251a1687de6167a14fdbcde887f37feb01776e
Sriram Panyam <sri.panyam@gmail.com>
2019-02-10 16:01:06 -0800

    Adding bundle files

---
+++ src/lib/bundles/index.js
@@ -0,0 +1,97 @@
+
+import * as triangle from "./triangle"
+import * as rect from "./rect"
+import * as square from "./square"
+import * as regpolygon from "./regpolygon"
+import * as star from "./star"
+import * as cog from "./cog"
+import * as rounded_rect from "./rounded_rect"
+import * as plus from "./plus"
+import * as circle from "./circle"
+import * as ellipse from "./ellipse"
+import * as left_arrow from "./left_arrow"
+import * as right_arrow from "./right_arrow"
+import * as double_arrow from "./double_arrow"
+import * as cylinder from "./cylinder"
+import * as left_block_arrow from "./left_block_arrow"
+import * as right_block_arrow from "./right_block_arrow"
+import * as left_chevron from "./left_chevron"
+import * as right_chevron from "./right_chevron"
+import * as straight_connector from "./straight_connector"
+import * as elbow_connector from "./elbow_connector"
+import * as curved_connector from "./curved_connector"
+import * as inmem_cache from "./inmem_cache"
+import * as dist_cache from "./dist_cache"
+import * as load_balancer from "./load_balancer"
+import * as web_server from "./web_server"
+import * as database from "./database"
+import * as btree_index from "./btree_index"
+import * as hash_index from "./hash_index"
+import * as kafka_queue from "./kafka_queue"
+import * as network_link from "./network_link"
+
+export const Bundle = {
+    "triangle": triangle,
+    "rect": rect,
+    "square": square,
+    "regpolygon": regpolygon,
+    "star": star,
+    "cog": cog,
+    "rounded_rect": rounded_rect,
+    "plus": plus,
+    "circle": circle,
+    "ellipse": ellipse,
+    "left_arrow": left_arrow,
+    "right_arrow": right_arrow,
+    "double_arrow": double_arrow,
+    "cylinder": cylinder,
+    "left_block_arrow": left_block_arrow,
+    "right_block_arrow": right_block_arrow,
+    "left_chevron": left_chevron,
+    "right_chevron": right_chevron,
+    "straight_connector": straight_connector,
+    "elbow_connector": elbow_connector,
+    "curved_connector": curved_connector,
+    "inmem_cache": inmem_cache,
+    "dist_cache": dist_cache,
+    "load_balancer": load_balancer,
+    "web_server": web_server,
+    "database": database,
+    "btree_index": btree_index,
+    "hash_index": hash_index,
+    "kafka_queue": kafka_queue,
+    "network_link": network_link
+}
+
+/**
+"triangle",
+"rect",
+"square",
+"regpolygon",
+"star",
+"cog",
+"rounded_rect",
+"plus",
+"circle",
+"ellipse",
+"left_arrow",
+"right_arrow",
+"double_arrow",
+"cylinder",
+"left_block_arrow",
+"right_block_arrow",
+"left_chevron",
+"right_chevron",
+"straight_connector",
+"elbow_connector",
+"curved_connector",
+"inmem_cache",
+"dist_cache",
+"load_balancer",
+"web_server",
+"database",
+"btree_index",
+"hash_index",
+"kafka_queue",
+"network_link",
+**/
---
+++ src/lib/bundles/square.js
@@ -0,0 +1,22 @@
+
+import * as core from "../core"
+
+export class Square extends core.Shape {
+    constructor(configs) {
+        super(configs);
+        var size = Math.min(this.bounds.width, this.bounds.height);
+        this.bounds._width = this.bounds._height = size;
+    }
+
+    draw(ctx) {
+        var size = Math.min(this.bounds.width, this.bounds.height);
+        var left = (this.bounds.left + this.bounds.right - size) / 2;
+        var top = (this.bounds.top + this.bounds.bottom - size) / 2;
+        if (this.fillStyle) {
+            ctx.fillRect(left, top, size, size);
+        }
+        if (this.lineWidth > 0) {
+            ctx.strokeRect(left, top, size, size);
+        }
+    }
+}
