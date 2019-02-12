489690d2b2c0b49201f30a3915dae04003f14218
Sriram Panyam <sri.panyam@gmail.com>
2019-02-11 23:47:51 -0800

    Plus implemented

--- src/lib/bundles/plus.js
+++ src/lib/bundles/plus.js
@@ -1,1 +1,63 @@
-Triangle.js
+
+
+import * as core from "../core"
+
+export function newShape(configs) {
+    return new PlusShape(configs);
+}
+
+export function newShapeForToolbar(configs) {
+    return new PlusShape(configs);
+}
+
+export class PlusShape extends core.Shape {
+    constructor(configs) {
+        super(configs);
+        this._innerWidth = configs.innerWidth || 0.3;
+        this._innerHeight = configs.innerHeight || 0.3;
+        this._controller = new PlusController(this);
+    }
+
+    get innerWidth() { return this._innerWidth; }
+    get innerHeight() { return this._innerHeight; }
+
+    draw(ctx) {
+        var lw = this.lineWidth + 2;
+        var x = this.bounds.x + lw;
+        var y = this.bounds.y + lw;
+        var width = this.bounds.width - (2 * lw);
+        var height = this.bounds.height - (2 * lw);
+        var iw = this.innerWidth * width;
+        var ih = this.innerHeight * height;
+
+        ctx.beginPath();
+        ctx.moveTo(x + (width - iw) / 2, y);
+        ctx.lineTo(x + (width + iw) / 2, y);
+        ctx.lineTo(x + (width + iw) / 2, y + (height - iw) / 2);
+        ctx.lineTo(x + width, y + (height - iw) / 2);
+        ctx.lineTo(x + width, y + (height + iw) / 2);
+        ctx.lineTo(x + (width + iw) / 2, y + (height + iw) / 2);
+        ctx.lineTo(x + (width + iw) / 2, y + height);
+        ctx.lineTo(x + (width - iw) / 2, y + height);
+        ctx.lineTo(x + (width - iw) / 2, y + (height + iw) / 2);
+        ctx.lineTo(x, y + (height + iw) / 2);
+        ctx.lineTo(x, y + (height - iw) / 2);
+        ctx.lineTo(x + (width - iw) / 2, y + (height - iw) / 2);
+        ctx.lineTo(x + (width - iw) / 2, y);
+        if (this.fillStyle) {
+            ctx.fill();
+        }
+        if (this.lineWidth > 0) {
+            ctx.stroke();
+        }
+    }
+}
+
+/**
+ * The controller responsible for handling updates and manipulations of the Shape.
+ */
+export class PlusController extends core.ShapeController {
+    constructor(shape) {
+        super(shape);
+    }
+}
