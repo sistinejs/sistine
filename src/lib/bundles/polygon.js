3cf0a19cc617d6e55e07177d2857ff016fd7e82f
Sriram Panyam <sri.panyam@gmail.com>
2019-02-11 23:28:40 -0800

    Resetting Polygon and Star shapes

--- src/lib/bundles/polygon.js
+++ src/lib/bundles/polygon.js
@@ -1,1 +1,63 @@
-Triangle.js
+
+import * as core from "../core"
+
+export function newShape(configs) {
+    return new PolygonShape(configs);
+}
+
+export function newShapeForToolbar(configs) {
+    return new PolygonShape(configs);
+}
+
+export class PolygonShape extends core.Shape {
+    constructor(configs) {
+        super(configs);
+        this._numSides = Math.max(3, configs.numSides || 5);
+        this._edgePoints = [];
+        this._controller = new PolygonController(this);
+    }
+
+    get numSides() {
+        return this._numSides;
+    }
+
+    draw(ctx) {
+        var n = this._numSides;
+        var theta = (Math.PI * 2.0) / n;
+        var cx = this.bounds.midX;
+        var cy = this.bounds.midY;
+        var radius = Math.min(this.bounds.width, this.bounds.height) / 2.0;
+        console.log("N, R, theta: ", n, radius, theta);
+
+        var fx = cx;
+        var fy = cy - radius;
+        ctx.beginPath();
+        ctx.moveTo(fx, fy);
+        console.log("Fx, Fy: ", fx, fy);
+        for (var i = 1;i < n;i++) {
+            var currangle = (Math.PI / 2.0) + (i * theta);
+            var costheta = Math.cos(currangle);
+            var sintheta = Math.sin(currangle);
+            var px = cx + (radius * costheta);
+            var py = cy - (radius * sintheta);
+            ctx.lineTo(px, py);
+            console.log("Px, Py: ", px, py);
+        }
+        ctx.lineTo(fx, fy);
+        if (this.fillStyle) {
+            ctx.fill();
+        }
+        if (this.lineWidth > 0) {
+            ctx.stroke();
+        }
+    }
+}
+
+/**
+ * The controller responsible for handling updates and manipulations of the Shape.
+ */
+export class PolygonController extends core.ShapeController {
+    constructor(shape) {
+        super(shape);
+    }
+}
--- src/lib/bundles/star.js
+++ src/lib/bundles/star.js
@@ -1,1 +1,73 @@
-Triangle.js
+
+
+import * as core from "../core"
+
+export function newShape(configs) {
+    return new StarShape(configs);
+}
+
+export function newShapeForToolbar(configs) {
+    return new StarShape(configs);
+}
+
+export class StarShape extends core.Shape {
+    constructor(configs) {
+        super(configs);
+        this._numSides = Math.max(3, configs.numSides || 5);
+        this._innerRadius = configs.innerRadius || (Math.min(this.bounds.width, this.bounds.height) / 5.0);
+        this._controller = new StarController(this);
+    }
+
+    get numSides() {
+        return this._numSides;
+    }
+
+    get innerRadius() {
+        return this._innerRadius;
+    }
+
+    draw(ctx) {
+        var n = 2 * this._numSides;
+        var theta = (Math.PI * 2.0) / n;
+        var cx = this.bounds.midX;
+        var cy = this.bounds.midY;
+        var R = Math.min(this.bounds.width, this.bounds.height) / 2.0;
+        var innerR = this._innerRadius;
+
+        var fx = cx;
+        var fy = cy - R;
+        ctx.beginPath();
+        ctx.moveTo(fx, fy);
+        for (var i = 1;i < n;i += 2) {
+            var currangle = (Math.PI / 2.0) + (i * theta);
+            var costheta = Math.cos(currangle);
+            var sintheta = Math.sin(currangle);
+            var px = cx + (innerR * costheta);
+            var py = cy - (innerR * sintheta);
+            ctx.lineTo(px, py);
+
+            var currangle = (Math.PI / 2.0) + ((i + 1) * theta);
+            var costheta = Math.cos(currangle);
+            var sintheta = Math.sin(currangle);
+            var px = cx + (R * costheta);
+            var py = cy - (R * sintheta);
+            ctx.lineTo(px, py);
+        }
+        ctx.lineTo(fx, fy);
+        if (this.fillStyle) {
+            ctx.fill();
+        }
+        if (this.lineWidth > 0) {
+            ctx.stroke();
+        }
+    }
+}
+
+/**
+ * The controller responsible for handling updates and manipulations of the Shape.
+ */
+export class StarController extends core.ShapeController {
+    constructor(shape) {
+        super(shape);
+    }
+}
