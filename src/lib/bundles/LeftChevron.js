71c644967b07b0406286d70f7b688be47ed52be1
Sriram Panyam <sri.panyam@gmail.com>
2019-02-24 19:04:04 -0800

    Starting on chevrons

--- src/lib/bundles/LeftChevron.js
+++ src/lib/bundles/LeftChevron.js
@@ -1,1 +1,16 @@
-Triangle.js
+
+import * as core from "../core"
+import * as leftArrows from "./LeftArrow"
+
+export function newShape(configs) {
+    configs = configs || {};
+    configs.shaftWidth = 1.0;
+    return new leftArrows.LeftArrowShape(configs);
+}
+
+export function newShapeForToolbar(configs) {
+    configs.y = configs.height / 4;
+    configs.height *= 0.6;
+    return newShape(configs);
+}
+
