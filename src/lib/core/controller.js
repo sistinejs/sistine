62d0429b0e5a794649c1ed4867bc6475c3230365
Sriram Panyam <sri.panyam@gmail.com>
2019-03-01 23:12:46 -0800

    Removed a log

--- src/lib/core/controller.js
+++ src/lib/core/controller.js
@@ -64,7 +64,6 @@ export class ShapeController {
             [[l, (t + b) / 2], "w-resize"],
             [[l, t], "nw-resize"],
         ]
-        console.log("gx, gy, x, y: ", gx, gy, x, y, ", Bounds: ", bounds);
         for (var i in sizePoints) {
             var hti = sizePoints[i];
             var px = hti[0][0];
