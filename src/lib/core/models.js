d55487f316a795953d05d048d97b00085e91797b
Sriram Panyam <sri.panyam@gmail.com>
2019-03-03 12:02:01 -0800

    Moving selection as a model entity and seperating out view related functionality as event handling

--- src/lib/core/models.js
+++ src/lib/core/models.js
@@ -696,3 +696,224 @@ export class Path extends Shape {
     }
 }
 
+export class Selection extends events.EventSource {
+    constructor() {
+        super();
+        this.shapes = {};
+        this.savedInfos = {};
+        this._count = 0;
+    }
+
+    get count() {
+        return this._count;
+    }
+
+    get allShapes() {
+        var out = [];
+        this.forEach(function(shape) {
+            out.push(shape);
+        });
+        return out;
+    }
+
+    forEach(handler, self, mutable) {
+        var shapes = this.shapes;
+        if (mutable == true) {
+            shapes = Object.assign({}, shapes);
+        }
+        for (var shapeId in shapes) {
+            var shape = shapes[shapeId];
+            if (handler(shape, self) == false)
+                break;
+        }
+    }
+
+    contains(shape) {
+        return shape.id in this.shapes;
+    }
+    
+    add(shape) {
+        // var event = new SelectionEvent("Selected", shape);
+        // if (this.validateBefore("ShapeSelected", event) != false) {
+            if ( ! (shape.id in this.shapes)) {
+                this._count ++;
+            }
+            this.shapes[shape.id] = shape;
+            this.savedInfos[shape.id] = shape.controller.snapshotFor();
+            this.triggerOn("ShapesSelected", event);
+        // }
+    }
+
+    remove(shape) {
+        // var event = new SelectionEvent("Unselected", shape);
+        // if (this.validateBefore("ShapeUnselected", event) != false) {
+            if ( shape.id in this.shapes ) {
+                this._count --;
+            }
+            delete this.shapes[shape.id];
+            delete this.savedInfos[shape.id];
+        // }
+    }
+
+    checkpointShapes(hitInfo) {
+        // Updated the save info for all selected shapes
+        this.forEach(function(shape, self) {
+            self.savedInfos[shape.id] = shape.controller.snapshotFor(hitInfo);
+        }, this);
+    }
+
+    getSavedInfo(shape) {
+        return this.savedInfos[shape.id];
+    }
+
+    toggleMembership(shape) {
+        if (shape == null) return false;
+        if (this.contains(shape)) {
+            this.remove(shape);
+            return false;
+        } else {
+            this.add(shape);
+            return true;
+        }
+    }
+
+    clear() {
+        this.triggerOn("ShapesUnselected", null);
+        this.forEach(function(shape, self) {
+            self.stage.setShapePane(shape, "main");
+        }, this);
+        this.savedInfos = {};
+        this.shapes = {};
+        this._count = 0;
+    }
+
+    /**
+     * Brings the selected shapes forward by one level within their parents.
+     */
+    bringForward() {
+        this.forEach(function(shape) {
+            shape.parent.bringForward(shape);
+        });
+    }
+
+    /**
+     * Sends the selected shapes backward by one level within their parents.
+     */
+    sendBackward() {
+        this.forEach(function(shape) {
+            shape.parent.sendBackward(shape);
+        });
+    }
+
+    /**
+     * Brings the selected shapes to the front of the stack within their parents.
+     */
+    bringToFront() {
+        this.forEach(function(shape) {
+            shape.parent.bringToFront(shape);
+        });
+    }
+
+    /**
+     * Sends the selected shapes to the back of the stack within their parents.
+     */
+    sendToBack() {
+        this.forEach(function(shape) {
+            shape.parent.sendToBack(shape);
+        });
+    }
+
+    /**
+     * Create a group out of the elements in this Selection.
+     */
+    group() {
+        // Collect all shapes in this selection
+        // Identify their parents.
+        // Do they all have the same parent?
+        // if all parents are "null" then they are all at the top level
+        // if they are all non null but same then they are all at teh same 
+        // level under the same parent so same as above and OK.
+        // But if different shapes have different parents then only
+        // those shapes that share a parent can be grouped together.
+        var groups = {};
+        this.forEach(function(shape) {
+            var parId = shape.parent;
+            if (parId) {
+                parId = shape.parent.id;
+            }
+            if (! (parId in groups)) {
+                groups[parId] = {
+                    parent: shape.parent,
+                    bounds: shape.bounds.copy(),
+                    shapes: []
+                };
+            }
+            groups[parId].shapes.push(shape);
+            groups[parId].bounds.union(shape.bounds);
+        });
+        console.log("Found Groups: ", groups);
+
+        this.clear();
+        for (var parentId in groups) {
+            var currGroup = groups[parentId];
+            var currBounds = currGroup.bounds;
+            var currParent = currGroup.parent;
+            // Here create a new shape group if we have atleast 2 shapes
+            if (currGroup.shapes.length > 1)  {
+                var newParent = new models.Group();
+                currParent.add(newParent);
+                newParent.setLocation(currBounds.x, currBounds.y);
+                newParent.setSize(currBounds.width, currBounds.height);
+                currGroup.shapes.forEach(function(child, index) {
+                    newParent.add(child);
+                    child.setLocation(child.bounds.x - currBounds.x, child.bounds.y - currBounds.y);
+                });
+                this.add(newParent);
+            }
+        }
+    }
+
+    /**
+     * Ungroups all elements in the current selection.  This is a no-op if number
+     * of elements in the selection is not 1 and the existing element is not a ShapeGroup.
+     */
+    ungroup() {
+        var selection = this;
+        this.forEach(function(shape, self) {
+            if (shape.isGroup) {
+                selection.remove(shape);
+                var newParent = shape.parent;
+                var bounds = shape.bounds;
+                shape.forEachChild(function(child, index, self) {
+                    newParent.add(child);
+                    child.setLocation(bounds.x + child.bounds.x,
+                                      bounds.y + child.bounds.y);
+                    selection.add(child);
+                }, this, true);
+                newParent.remove(shape);
+            }
+        }, this, true);
+    }
+
+    /**
+     * Regroups elements in the selection.  This is useful if elements are added after
+     * grouping and we want to add to existing groups consolidating multiple groups
+     * into a single group.
+     */
+    regroup() {
+    }
+
+    /**
+     * "Copies" the shapes in this selection to the clipboard along with their current state
+     * so that it can be pasted later.   The "cut" parameter also dictates whether the
+     * selected shapes are to be removed from the Scene model too.
+     */
+    copyToClipboard(cut) {
+    }
+
+    /**
+     * Paste a copy of shapes stored in the clipboard.
+     */
+    pasteFromClipboard() {
+    }
+}
