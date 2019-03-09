a9a1d9eebb0f41e3778fc9a591b56b669c35ee9a
Sriram Panyam <sri.panyam@gmail.com>
2019-03-09 14:40:11 -0800

    Using ejs and hbs templates to include templates within each other

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = (env, options) => {
         // new uglifyJsPlugin(),
         // new CleanWebpackPlugin('dist'),
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/demo/index.html')
+            // template: path.resolve(__dirname, 'src/demo/index.html')
+            template: path.resolve(__dirname, 'src/demo/index.ejs')
         }),
         new HtmlWebpackIncludeAssetsPlugin({
             assets: [
@@ -66,6 +67,60 @@ module.exports = (env, options) => {
         output: output,
         module: {
             rules: [
+                // The rule for rendering index.html from an ejs template.
+                {
+                  test: /\/src\/demo\/index.ejs$/,
+                  use: [{
+                    loader: 'extract-loader'
+                  },
+                  {
+                    loader: 'html-loader',
+                    options: {
+                      interpolate: 'require'
+                    }
+                  },
+                  {
+                    loader: 'render-template-loader',
+                    options: {
+                      engine: 'ejs',
+                      locals: {
+                        title: 'Render Template Loader',
+                        desc: 'Rendering templates with a Webpack loader since 2017'
+                      },
+                      engineOptions: function (info) {
+                        // Ejs wants a filename for partials rendering.
+                        // (Configuring a "views" option can also be done.)
+                        return { filename: info.filename }
+                      }
+                    }
+                  }]
+                },
+                // The rule for rendering page-hbs.html from a handlebars template.
+                {
+                  test: /\.hbs$/,
+                  use: [{
+                    loader: 'file-loader?name=[name]-[ext].html'
+                  },
+                  {
+                    loader: 'extract-loader'
+                  },
+                  {
+                    loader: 'render-template-loader',
+                    options: {
+                      engine: 'handlebars',
+                      init: function (engine, info) {
+                        engine.registerPartial(
+                          'body',
+                          fs.readFileSync('./src/body.hbs').toString()
+                        )
+                      },
+                      locals: {
+                        title: 'Rendered with Handlebars!',
+                        desc: 'Partials Support'
+                      },
+                    }
+                  }]
+                },
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
