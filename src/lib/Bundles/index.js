5e7ad9b68dd1b14fa771d4bcabc404bf3feb970c
Sriram Panyam <sri.panyam@gmail.com>
2019-03-23 15:12:31 -0700

    Removing shapes not yet created

--- src/lib/Bundles/index.js
+++ src/lib/Bundles/index.js
@@ -4,7 +4,6 @@ import * as Rectangle from "./Rectangle"
 import * as Square from "./Square"
 import * as Polygon from "./Polygon"
 import * as Star from "./Star"
-import * as Cog from "./Cog"
 import * as RoundedRectangle from "./RoundedRectangle"
 import * as Plus from "./Plus"
 import * as Circle from "./Circle"
@@ -17,18 +16,6 @@ import * as LeftBlockArrow from "./LeftBlockArrow"
 import * as RightBlockArrow from "./RightBlockArrow"
 import * as LeftChevron from "./LeftChevron"
 import * as RightChevron from "./RightChevron"
-import * as StraightConnector from "./StraightConnector"
-import * as ElbowConnector from "./ElbowConnector"
-import * as CurvedConnector from "./CurvedConnector"
-import * as InMemCache from "./InMemCache"
-import * as DistributedCache from "./DistributedCache"
-import * as LoadBalancer from "./LoadBalancer"
-import * as WebServer from "./WebServer"
-import * as Database from "./Database"
-import * as BTreeIndex from "./BTreeIndex"
-import * as HashIndex from "./HashIndex"
-import * as KafkaQueue from "./KafkaQueue"
-import * as NetworkLink from "./NetworkLink"
 
 export const Bundle = {
     "Triangle": Triangle,
@@ -36,7 +23,6 @@ export const Bundle = {
     "Square": Square,
     "Polygon": Polygon,
     "Star": Star,
-    "Cog": Cog,
     "RoundedRectangle": RoundedRectangle,
     "Plus": Plus,
     "Circle": Circle,
@@ -49,18 +35,6 @@ export const Bundle = {
     "RightBlockArrow": RightBlockArrow,
     "LeftChevron": LeftChevron,
     "RightChevron": RightChevron,
-    "StraightConnector": StraightConnector,
-    "ElbowConnector": ElbowConnector,
-    "CurvedConnector": CurvedConnector,
-    "InMemCache": InMemCache,
-    "DistributedCache": DistributedCache,
-    "LoadBalancer": LoadBalancer,
-    "WebServer": WebServer,
-    "Database": Database,
-    "BTreeIndex": BTreeIndex,
-    "HashIndex": HashIndex,
-    "KafkaQueue": KafkaQueue,
-    "NetworkLink": NetworkLink,
 }
 
 /*
