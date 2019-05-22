6d96ad4ff86468a2b2bb1c395c1990c7039e4126
Sriram Panyam <sri.panyam@gmail.com>
2019-05-22 07:32:10 -0700

    updated docs

--- src/lib/Core/base.js
+++ src/lib/Core/base.js
@@ -3,6 +3,15 @@ import * as events from "./events";
 
 const ElementCounter = new counters.Counter("ElementIDs");
 
+/**
+ * A property instance.  
+ * Properties enable capturing some common behaviours for attributes 
+ * on objects like inheritance from parents, default values, updated 
+ * timestamps and change notifications.
+ * @param {string}   name   Name of the property (used only for debugging).
+ * @param {Value}    value  Value of the property.  If the value is undefined 
+ *                          then it is marked as "to be inherited".
+ */
 export class Property {
     constructor(name, value) {
         this.name = name;
@@ -10,10 +19,22 @@ export class Property {
         this.set(value);
     }
 
+    /**
+     * Clones the property instance.
+     * @returns  {Property}     A shallow copy of this property with the same name, 
+     *                          value and inherit flags.
+     */
     clone() {
         return new Property(this.name, this.value);
     }
 
+    /**
+     * Sets the value of the property and also in the process notifies listeners (if any) of changes.
+     * @param   {value}         newValue    The new value to set this property to.
+     * @param   {EventSource}   eventSource The event source, if any, on which to notify listeners of changes.
+     *
+     * @returns {Bool}          true if the change was applied, false if change rejected by the eventSource's validation.
+     */
     set(newValue, eventSource) {
         var oldValue = this.vaue;
         if (oldValue == newValue) 
@@ -34,24 +55,37 @@ export class Property {
         return true;
     }
 
+    /**
+     * Sets the property's updated time stamp to now.
+     */
     markUpdated() { this.lastUpdated = Date.now(); }
 }
 
+/**
+ * Base of all Element nodes in a virtual document tree.  Elements allow for unique IDs
+ * for instances, to have meta data and (ofcourse) child elements.  Elements play a key
+ * role in building and maintaining Scenes with hierarchical shapes.
+ */
 export class Element extends events.EventSource {
     constructor() {
         super();
         this._uuid = ElementCounter.next();
         this._parent = null;
         this._defs = {};
-        this._titles = [];
-        this._descriptions = [];
         this._metadata = {};
-        this._miscdata = {};
-        var self = this;
     }
 
+    /**
+     * Returns a new instance of this element when cloning it.
+     * @return {Element} 
+     */
     newInstance() { return new this.constructor(); };
 
+    /**
+     * Performs a shallow copy of this element by creating a new instance of it
+     * and copying over the children.
+     * @return {Element} 
+     */
     clone() {
         var out = this.newInstance();
         for (var i = 0;i < this.childCount;i++) {
@@ -61,16 +95,34 @@ export class Element extends events.EventSource {
         return out;
     }
 
+    /**
+     * Indicates whether a parent exists.
+     *
+     * @return {Bool} true if parent exists, false otherwise.
+     */
     get hasParent() { return this._parent != null; }
 
-    forEachChild(handler, self, mutable) {
+    /**
+     * A visitor method over the children.
+     *
+     * @param {Function<Element, int, TypeOf<This>>} handler
+     *          A handler method to process each child.  If the handler returns a false, looping is stopped.
+     * @param {Object}  caller
+     *          In a typical callback pattern, the caller could be an object that will have to be scoped
+     *          with a "self".  By passing the caller into this method, this creation of a local "self"
+     *          variable can be avoided for cleanliness.
+     * @param {Bool}  mutable
+     *          If mutable is true, then modifications of the children is allowed.  This is done by
+     *          making a copy of the child elements before looping over them.
+     */
+    forEachChild(handler, caller, mutable) {
         var children = this._children;
         if (mutable == true) {
             children = children.slice(0, children.length);
         }
         for (var index in children) {
             var child = children[index];
-            if (handler(child, index, self) == false)
+            if (handler(child, index, caller) == false)
                 break;
         }
     }
@@ -81,18 +133,56 @@ export class Element extends events.EventSource {
     inherit(property) {
     }
 
+    /**
+     * Tells if a given property is inherited or not.
+     */
     isInherited(property) {
     }
 
+    /**
+     * Returns the unique ID of this instance.
+     * @returns {long}   Unique ID of this instance.
+     */
     get uuid() { return this._uuid; }
 
-    addTitle(t) { this._titles.push(t); }
-    addDescription(d) { this._descriptions.push(d); }
+    /**
+     * Gets a particular metadata entry.
+     *
+     * @param {String} key   Key of the entry to return.
+     *
+     * @returns {Object} Value of the key if it exists, null otherwise.
+     */
     getMetaData(key) { return this._metadata[key] || null; }
+
+    /**
+     * Sets a particular metadata entry.
+     *
+     * @param {String} key      Key of the entry to set.
+     * @param {Object} value    Value of the entry to set.
+     *
+     * @returns {TypeOf<this>} This instance.
+     */
     setMetaData(key, value) { this._metadata[key] = value; return this; }
-    getMiscData(key) { return this._miscdata[key] || null; }
-    setMiscData(key, value) { this._miscdata[key] = value; return this; }
-    addDef(id, value) { this._defs[id] = value; }
+
+    /**
+     * Add a definition for a particular ID.  Definitions allow entries to
+     * be referenced and reused without being duplicated.  Particularly useful
+     * for pattern definitions for stroke and fill styles.
+     *
+     * @param {String}   id      ID of the entry being defined.
+     * @param {Object}   value   Value of the entry being defined.
+     * @returns {TypeOf<this>}   This instance.
+     */
+    addDef(id, value) { this._defs[id] = value; return this; }
+
+    /**
+     * Returns the definition for a particular ID.  
+     * If the definition does not exist in this element, the parent (and so on) is looked 
+     * up until one is found or the root is reached.
+     * 
+     * @param {String}  id  ID of the definition to be looked up.
+     * @returns {Object}  Value of the definition within the closest ancestor, null if no entry found.
+     */
     getDef(id) { 
         if (id in this._defs) {
             return this._defs[id];
@@ -102,17 +192,33 @@ export class Element extends events.EventSource {
         return null;
     }
 
+    /**
+     * Sets the last udpated timestamp of this element to now.
+     */
     markUpdated() { this._lastUpdated = Date.now(); }
 
+    /**
+     * Returns if the element has any children.
+     */
     get hasChildren() { return false; }
+
+    /**
+     * Returns the child count of this element.
+     */
     get childCount() { return 0; } 
 
+    /**
+     * Returns the parent of this eleemnt.
+     */
     get parent() { return this._parent; } 
 
     /**
      * Adds a new element to this group.
-     * Returns true if a element was successfully added
-     * false if the addition was blocked.
+     * As part of the addition, the {ElementAdded} event is also fired for both
+     * the parent as well as the element being added for more finegrained and efficient
+     * filtering of events.
+     *
+     * @returns {Bool} true if a element was successfully added false if the addition was blocked.
      */
     add(element, index) {
         index = index || -1;
